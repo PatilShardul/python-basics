ae0ca3e0fceee0a415ec6c2c8cde389c94a6216f
Shardul Patil <patilshardulsatish@yahoo.com>
2020-12-25 12:06:05 +0530

    Add : created a flip_coin module to find percentage of flip in heads and tails

--- basic_core_programs/flip_coin_problem.py
+++ basic_core_programs/flip_coin_problem.py
@@ -7,25 +7,25 @@
 
 import random
 
-number_of_heads=0
-number_of_tails=0
-flip_count=0
-heads_percentage=0
-tails_percentage=0
+number_of_heads = 0
+number_of_tails = 0
+flip_count = 0
+heads_percentage = 0
+tails_percentage = 0
 
-try:
-
-    TOTAL_COIN_FLIP=int(input("Enter the total number of coin flip expected : "))     
+try:     
     
-    while (( TOTAL_COIN_FLIP > 0 ) && ( flip_count <= TOTAL_COIN_FLIP )) :  
+    TOTAL_COIN_FLIP = int(input("Enter the total number of coin flip expected : "))    
+    
+    while ( TOTAL_COIN_FLIP > 0 ) & ( flip_count < TOTAL_COIN_FLIP ):  
         
-        flip_count=+1
+        flip_count+=1
         result=random.uniform(0,1)
     
         if(result>0.5):
-            number_of_heads=+1
+            number_of_heads+=1
         else:
-            number_of_tails=+1
+            number_of_tails+=1
 
     heads_percentage=((number_of_heads/TOTAL_COIN_FLIP)*100)
     tails_percentage=((number_of_tails/TOTAL_COIN_FLIP)*100)
